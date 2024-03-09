d1afa7fa3d57b57f06a5eed58410064d434619a2
Rick South <rick@natamus.com>
2024-03-09 11:55:55 +0100

    (v2.0) Added the slowness effect to the horse and player as the default when riding without a saddle.

--- Common/src/main/java/com/natamus/barebackhorseriding/util/Util.java
+++ Common/src/main/java/com/natamus/barebackhorseriding/util/Util.java
@@ -2,7 +2,11 @@ package com.natamus.barebackhorseriding.util;
 
 import com.natamus.collective.functions.EntityFunctions;
 import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.animal.horse.AbstractHorse;
+import net.minecraft.world.entity.animal.horse.Horse;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 
@@ -13,6 +17,17 @@ public class Util {
 
 	public static boolean isActuallyWearingASaddle(AbstractHorse abstractHorse) {
 		return EntityFunctions.getAbstractHorseEntityFlagResult(abstractHorse, 4);
+	}
+
+	public static void giveSlowness(Player player) {
+		player.addEffect(new MobEffectInstance(new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 40, 0)));
+
+		Entity vehicle = player.getVehicle();
+		if (vehicle instanceof Horse) {
+			Horse horse = (Horse)vehicle;
+
+			horse.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 40, 0));
+		}
 	}
 
 	public static void damagePlayer(Player player, int halfheartdamage) {
