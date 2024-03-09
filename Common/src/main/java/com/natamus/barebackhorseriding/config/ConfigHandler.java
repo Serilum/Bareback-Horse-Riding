f6612cffb91c57c3fb1c7477caeadc6c4227883f
Rick South <rick@natamus.com>
2024-03-09 11:55:59 +0100

    (v2.0) Added the slowness effect to the horse and player as the default when riding without a saddle.

--- Common/src/main/java/com/natamus/barebackhorseriding/config/ConfigHandler.java
+++ Common/src/main/java/com/natamus/barebackhorseriding/config/ConfigHandler.java
@@ -10,18 +10,22 @@ import java.util.List;
 public class ConfigHandler extends DuskConfig {
 	public static HashMap<String, List<String>> configMetaData = new HashMap<String, List<String>>();
 
-	@Entry public static boolean shouldDamageDuringRidingWithoutSaddle = true;
-	@Entry public static boolean leatherPantsNegateDamage = true;
+	@Entry public static boolean shouldReceiveSlownessDuringRidingWithoutSaddle = true;
+	@Entry public static boolean shouldDamageDuringRidingWithoutSaddle = false;
+	@Entry public static boolean leatherPantsNegateEffect = true;
 	@Entry(min = 1, max = 72000) public static int ticksBetweenDamage = 300;
 	@Entry(min = 1, max = 100) public static int halfHeartDamageAmount = 2;
 	@Entry public static boolean leatherPantsLoseDurabilityOnNegation = true;
 
 	public static void initConfig() {
+		configMetaData.put("shouldReceiveSlownessDuringRidingWithoutSaddle", Arrays.asList(
+			"If the player (and horse) should receive the slowness effect when riding a horse without a saddle."
+		));
 		configMetaData.put("shouldDamageDuringRidingWithoutSaddle", Arrays.asList(
 			"If the player should be damaged when riding a horse without a saddle."
 		));
-		configMetaData.put("leatherPantsNegateDamage", Arrays.asList(
-			"If wearing leather pants should prevent the player from being damaged, if enabled, when riding without a saddle."
+		configMetaData.put("leatherPantsNegateEffect", Arrays.asList(
+			"If wearing leather pants should prevent the player from being damaged or received slowness, if enabled, when riding without a saddle."
 		));
 		configMetaData.put("ticksBetweenDamage", Arrays.asList(
 			"How often the player should be damaged, if enabled, during riding without a saddle in ticks. 1 second = 20 ticks"
