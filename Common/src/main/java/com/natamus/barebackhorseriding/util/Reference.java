432426cb2fa93ca5b1beec3649b39b265e71164c
Rick South <rick@natamus.com>
2024-06-14 05:43:20 +0200

    Bareback Horse Riding source code for 1.21.0.

---
+++ Common/src/main/java/com/natamus/barebackhorseriding/util/Reference.java
@@ -0,0 +1,8 @@
+package com.natamus.barebackhorseriding.util;
+
+public class Reference {
+	public static final String MOD_ID = "barebackhorseriding";
+	public static final String NAME = "Bareback Horse Riding";
+	public static final String VERSION = "2.1";
+	public static final String ACCEPTED_VERSIONS = "[1.21.0]";
+}
---
+++ Fabric/src/main/java/com/natamus/barebackhorseriding/ModFabric.java
@@ -0,0 +1,32 @@
+package com.natamus.barebackhorseriding;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.fabric.callbacks.CollectivePlayerEvents;
+import com.natamus.barebackhorseriding.events.RidingEvent;
+import com.natamus.barebackhorseriding.util.Reference;
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+
+public class ModFabric implements ModInitializer {
+	
+	@Override
+	public void onInitialize() {
+		setGlobalConstants();
+		ModCommon.init();
+
+		loadEvents();
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadEvents() {
+		CollectivePlayerEvents.PLAYER_TICK.register((ServerLevel world, ServerPlayer player) -> {
+			RidingEvent.onPlayerTick(world, player);
+		});
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}
