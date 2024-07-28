15c249edbeaba62394d1e31acfdb56d246d992c9
Rick South <rick@natamus.com>
2024-07-28 07:43:27 +0200

    (v2.2) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- NeoForge/src/main/java/com/natamus/barebackhorseriding/ModNeoForge.java
+++ NeoForge/src/main/java/com/natamus/barebackhorseriding/ModNeoForge.java
@@ -1,6 +1,7 @@
 package com.natamus.barebackhorseriding;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.barebackhorseriding.neoforge.config.IntegrateNeoForgeConfig;
 import com.natamus.barebackhorseriding.neoforge.events.NeoForgeRidingEvent;
 import com.natamus.barebackhorseriding.util.Reference;
@@ -15,6 +16,10 @@ import net.neoforged.fml.javafmlmod.FMLJavaModLoadingContext;
 public class ModNeoForge {
 	
 	public ModNeoForge(IEventBus modEventBus) {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		modEventBus.addListener(this::loadComplete);
 
 		setGlobalConstants();
